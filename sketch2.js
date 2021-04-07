4ee2cdeca29b00ca26e0edc1fea803ad1ec1ac91
FurtadoShane <73696311+FurtadoShane@users.noreply.github.com>
2021-04-07 16:58:29 +0530

    Add files via upload

--- sketch2.js
+++ sketch2.js
@@ -1,20 +1,24 @@
 var bullet, speed, weight;
 var wall, thickness;
 
-thickness=random(22,83);
-speed=random(233,321);
-weight=random(30,52);    
+   
 
 function setup(){
    createCanvas(1600,400);
 
-   bullet.velocityX=speed;
-   wall=createSprite(1200,200,thickness,height/2);
+   thickness=random(22,83);
+    speed=random(233,321);
+    weight=random(30,52); 
 
+   wall=createSprite(1200,200,thickness,height/2);
+   wall.shapeColor="black";
+   bullet=createSprite(50,200,50,5);
+   bullet.velocityX=speed;
+   bullet.shapeColor="yellow";
 }
 
 function draw(){
-    background(80,80,80);
+    background(255);
 
     if(hasCollided(bullet,wall)){
         bullet.velocityX=0;
@@ -22,11 +26,11 @@ function draw(){
         var damage=0.5*weight*speed*speed/thickness*thickness*thickness;
 
         if(damage>=10){
-            wall.shapecolor= color(250,0,0);
+            wall.shapeColor= color(250,0,0);
         }
 
         if(damage<10){
-            wall.shapecolor= color(0,250,0);
+            wall.shapeColor= color(0,250,0);
         }
     }
 
